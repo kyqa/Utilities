92ad91fb3f84bbbf8fd305087e1250e7ddd5f32c
kyqa <126547641+kyqa@users.noreply.github.com>
2023-07-16 04:27:22 -0400

    Update Utilities.lua

--- Utilities.lua
+++ Utilities.lua
@@ -70,18 +70,9 @@ function SetJersey(player, teamInfo, pos)
             end
 
             --Setting Helmet
-            local player = game.Players.LocalPlayer
-            local homeTeam = module.Settings.HomeInfo
-            local awayTeam = module.Settings.AwayInfo
-
-            if homeTeam and player.Team == homeTeam then
-                uniform.Helmet.Mesh.TextureId = "rbxassetid://14082617721"
-                uniform.Helmet.RightLogo.Decal.Texture = "rbxassetid://14082583575"
-                uniform.Helmet.LeftLogo.Decal.Texture = "rbxassetid://14082583575"
-            elseif awayTeam and player.Team == awayTeam then
-                uniform.Helmet.Mesh.TextureId = "rbxassetid://14082618845"
-                uniform.Helmet.RightLogo.Decal.Texture = "rbxassetid://14082582244"
-                uniform.Helmet.LeftLogo.Decal.Texture = "rbxassetid://14082582244"
+            uniform.Helmet.Mesh.TextureId = (teamInfo["Colors"]["Jersey"][pos]["Jersey"])
+            uniform.Helmet.RightLogo.Decal.Texture = (teamInfo["Colors"]["Jersey"][pos]["Jersey"])
+            uniform.Helmet.LeftLogo.Decal.Texture = (teamInfo["Colors"]["Jersey"][pos]["Jersey"])
 
             --Setting Upper Uniform
             uniform.ShoulderPads.Front.Team.Text = string.upper(teamInfo["Name"])
