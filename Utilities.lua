b3aaaffcb95f51dc904063b0605fa638207de7c6
kyqa <126547641+kyqa@users.noreply.github.com>
2023-07-20 20:54:48 -0400

    Update Utilities.lua

--- Utilities.lua
+++ Utilities.lua
@@ -72,12 +72,12 @@ function SetJersey(player, teamInfo, pos)
             --Setting Helmet
             uniform.Helmet.Mesh.TextureId = (teamInfo["Colors"]["Jersey"][pos]["HelmetTexture"])
 
-            if (FFValues.StatusTag.Value != "REPLAY") then
-                pass
+            if (FFValues.StatusTag.Value ~= "REPLAY") then
+                return
             else
                 uniform.Helmet.RightLogo.Decal.Texture = (teamInfo["Colors"]["Jersey"][pos]["Logo"])
                 uniform.Helmet.LeftLogo.Decal.Texture = (teamInfo["Colors"]["Jersey"][pos]["Logo"])
-
+            end
             --Setting Upper Uniform
             uniform.ShoulderPads.Front.Team.Text = string.upper(teamInfo["Name"])
             uniform.ShoulderPads.Color = Color3.fromHex(teamInfo["Colors"]["Jersey"][pos]["Jersey"])
